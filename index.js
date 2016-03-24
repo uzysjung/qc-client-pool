fd42612965a82f59df09fe27dbca20de3af59eeb
Uzys <uzysjung@gmail.com>
2016-03-24 18:42:53 +0900

    Pool Release bug fixed

--- index.js
+++ index.js
@@ -80,46 +80,45 @@ internals.qcPool.prototype.query = function(connection,sql,option) {
     var self = this;
     return new Promise(function(resolve,reject){
         co(function*(){
-            try {
-                let results = [];
-                let stmt = connection.createStatement();
-                let hasResultSet = yield stmt.execute(sql);
-                if (!hasResultSet) {
-                    throw new Error("query affected " + stmt.updateRowCount + " rows.");
-                }
-
-                let resultSet = yield stmt.getResultSet();
-                if (resultSet == null) {
-                    throw new Error("query has no result set. (BUG?)");
-                }
-
-                let rows = 0;
-                for(;;) {
-                    const nextRowAvailable = yield resultSet.next();
-                    if (nextRowAvailable) {
-                        rows++;
-                        if(option && option.rowResultType == "Dictionary")
-                            results.push(resultSet.getRowDict());
-                        else
-                            results.push(resultSet.getRowArray());
-                    }
-                    else {
-                        break;
-                    }
-                }
-
-                if (resultSet) {
-                    yield resultSet.close();
-                }
-                if (stmt) {
-                    yield stmt.close();
-                }
-                self.pool.release(connection);
-                resolve(results);
-
-            } catch(e) {
-                reject(e);
+            let results = [];
+            let stmt = connection.createStatement();
+            let hasResultSet = yield stmt.execute(sql);
+            if (!hasResultSet) {
+                throw new Error("query affected " + stmt.updateRowCount + " rows.");
             }
+
+            let resultSet = yield stmt.getResultSet();
+            if (resultSet == null) {
+                throw new Error("query has no result set. (BUG?)");
+            }
+
+            let rows = 0;
+            for(;;) {
+                const nextRowAvailable = yield resultSet.next();
+                if (nextRowAvailable) {
+                    rows++;
+                    if(option && option.rowResultType == "Dictionary")
+                        results.push(resultSet.getRowDict());
+                    else
+                        results.push(resultSet.getRowArray());
+                }
+                else {
+                    break;
+                }
+            }
+
+            if (resultSet) {
+                yield resultSet.close();
+            }
+            if (stmt) {
+                yield stmt.close();
+            }
+            self.pool.release(connection);
+            resolve(results);
+
+        }).catch(function(e){
+            self.pool.release(connection);
+            reject(e);
         });
     });
 };
